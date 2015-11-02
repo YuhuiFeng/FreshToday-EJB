24e61a69b2e4cb58489ee427630eef2e943d36db
Yuhui Feng <fengyuhui@FelixdeMacBook-Pro.local>
2015-11-02 18:57:50 -0500

    Lab 5 in session

--- ejbModule/edu/osu/cse5234/business/InventoryServiceBean.java
+++ ejbModule/edu/osu/cse5234/business/InventoryServiceBean.java
@@ -5,9 +5,11 @@ import edu.osu.cse5234.business.view.InventoryService;
 import edu.osu.cse5234.business.view.Item;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.*;
 
 /**
  * Session Bean implementation class InventoryServiceBean
@@ -23,15 +25,43 @@ public class InventoryServiceBean implements InventoryService {
         // TODO Auto-generated constructor stub
     }
 
+    @PersistenceContext private EntityManager entityManager;
+    
+    private static final String MY_QUERY = "select i from Item i";
+//    private Item item;
+    private Inventory inventory = new Inventory();
+    
+//    public void setItemID(int id) {
+//    	item = entityManager.find(Item.class, id);	
+//    	Inventory inventory = Inventory.getInstance();
+//    	inventory.add(item);
+//    }
+    
 	@Override
 	public Inventory getAvailableInventory() {
 		// TODO Auto-generated method stub
-		return Inventory.getInstance();
+//		Inventory inventory = new Inventory();
+		inventory.setItems(entityManager.createQuery(MY_QUERY, Item.class).getResultList());
+		return inventory;
 	}
 
 	@Override
 	public boolean validateQuantity(Collection<Item> items) {
 		// TODO Auto-generated method stub
+		List<Item> orderItems = (List<Item>) items;
+		Inventory inventory_origin = getAvailableInventory();
+		for(int i=0; i<items.size(); i++){
+			Item orderOne = orderItems.get(i);
+			for(int j=0; j<inventory_origin.size(); j++){
+				Item inventoryOne = inventory_origin.getInventory().get(j);
+				if(orderOne.getId()==inventoryOne.getId()){
+					if(Integer.parseInt(orderOne.getQuantity())>Integer.parseInt(inventoryOne.getQuantity())){
+						return false;
+					}
+					break;
+				}
+			}
+		}
 		return true;
 	}
 
